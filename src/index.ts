61d7934eab8f1e2f2e99308989c3b9a749bc0a24
Martin Küchler <mkuechle@hs-mittweida.de>
2019-01-16 09:45:05 +0100

    exported Erc20TestToken and TestReceiver

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Erc20TestTokenJSON from '../contract-build/Erc20TestToken.json';
 import Erc721TestReceiverJSON from '../contract-build/TestReceiver.json';
 import { deployERC20TestToken, deployERC721TestReceiver } from './deployment/deploy';
+import { Erc20TestToken } from './wrappedContracts/Erc20TestToken';
+import { TestReceiver } from './wrappedContracts/TestReceiver';
 
-export { Erc20TestTokenJSON, Erc721TestReceiverJSON, deployERC20TestToken, deployERC721TestReceiver };
+export { Erc20TestTokenJSON, Erc721TestReceiverJSON, deployERC20TestToken, deployERC721TestReceiver, Erc20TestToken, TestReceiver };
